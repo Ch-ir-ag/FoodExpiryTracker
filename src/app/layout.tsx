52c44c4994164643848c00847f9069fd4fae7e5e
Ch-ir-ag <csjoshi2003@gmail.com>
2025-03-19 19:40:43 +0000

    Added microsoft clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { AuthProvider } from '@/contexts/AuthContext';
 import Navbar from '@/components/Navbar';
 import { Toaster } from 'react-hot-toast';
 import { Analytics } from "@vercel/analytics/react";
+import Script from 'next/script';
 
 const inter = Inter({
   subsets: ['latin'],
@@ -23,6 +24,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={inter.variable}>
+      <head>
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "qqt5hsmw3s");
+          `}
+        </Script>
+      </head>
       <body className="font-sans antialiased">
         <AuthProvider>
           <Navbar />
